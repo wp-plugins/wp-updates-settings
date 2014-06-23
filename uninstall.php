30c642a632252283130f925a918c56c695fa5d6e
Yslo <Yslo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-23 08:41:53 +0000

    1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-updates-settings/trunk@936996 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -9,12 +9,3 @@
 if( !defined('WP_UNINSTALL_PLUGIN') ) exit();
 
 delete_option('yslo_wpus_options');
-
-if(current_user_can('delete_plugins')){	
-	$role =& get_role('administrator');
-	if(!empty($role)) {
-			$role->add_cap('update_core');
-			$role->add_cap('update_themes');
-			$role->add_cap('update_plugins');
-	}
-}
